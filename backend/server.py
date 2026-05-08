f1795fc280afdce3e7ff5dfaf5d94b9ee9472262
vasj0699-oss <vasj0699@gmail.com>
2026-05-08 01:29:28 -0600

    fix

--- backend/server.py
+++ backend/server.py
@@ -1038,7 +1038,11 @@ app.include_router(api)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.environ.get("CORS_ORIGINS", "*").split(","),
+    allow_origins=[
+        os.environ.get("FRONTEND_URL", "*"),
+        "http://localhost:3000",
+        "*",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
